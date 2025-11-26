42ea2006a8bbe56928ca80b4209ebcfae647e8bc
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-26 14:14:34 +0800

    feat: setup http server

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+		Addr:           ":" + port,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Printf("Serving on port: %v\n", port)
+	log.Fatal(s.ListenAndServe())
 }
