99ca8a0ad7a64ce0d7a13062e780bff7a559d233
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-28 13:49:58 +0800

    feat: output html from handlerMetrics()

--- metrics.go
+++ metrics.go
@@ -7,10 +7,17 @@ import (
 )
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	hits := cfg.fileserverHits.Load()
-	body := fmt.Sprintf("Hits: %v", hits)
+	body := fmt.Sprintf(`
+<html>
+    <body>
+	      <h1>Welcome, Chirpy Admin</h1>
+	      <p>Chirpy has been visited %v times!</p>
+    </body>
+<html>
+		`, hits)
 	_, err := w.Write([]byte(body))
 	if err != nil {
 		log.Fatal("Failed to write http body")
