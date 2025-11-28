aa7b255727d9cbdb035504550a96d4790d6eccb0
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-28 13:17:19 +0800

    feat: add metrics tracking
    - middleware func. to wrap main /app/ pattern, inc. each request
    - handlerMetrics func. to report the number of hits
    - handlerReset func. to reset the number of hits

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
@@ -19,8 +27,12 @@ func main() {
 		if err != nil {
 			log.Fatal("Failed to write http body")
 		}
+	}
+	mux.Handle("/app/", cfg.metricsMiddleware(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerReset)
 
-	})
 
 	s := &http.Server{
 		Addr:           ":" + port,
@@ -32,4 +44,21 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(s.ListenAndServe())
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	body := fmt.Sprintf("Hits: %v", hits)
+	_, err := w.Write([]byte(body))
+	if err != nil {
+		log.Fatal("Failed to write http body")
+	}
+}
+
+func (cfg *apiConfig) metricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, r)
+	})
 }
