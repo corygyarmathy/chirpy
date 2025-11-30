f4edf7e725f2991ca002e75c3145fbd63b73e55f
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-30 21:08:27 +0800

    feat(db): add pq driver, UUID dep., and db connection

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 	"time"
+
+	"github.com/corygyarmathy/chirpy/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable must be set")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("DB SQL open error: %v\n", err)
+	}
+	defer func() {
+		if cerr := db.Close(); cerr != nil && err == nil {
+			log.Fatalf("DB error: %v", cerr)
+		}
+	}()
+
+	dbQueries := database.New(db)
 	cfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
 	}
 
 	mux := http.NewServeMux()
