e224992f474ee5f61d8eeee77593f47c5e5772b2
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-12-01 17:02:26 +0800

    refactor: use http.Error() to cleanup Reset()

--- internal/handlers/reset.go
+++ internal/handlers/reset.go
@@ -6,30 +6,22 @@ import (
 )
 
 func (api *API) Reset(w http.ResponseWriter, r *http.Request) {
-	statusCode := http.StatusOK
-	response := []byte("Hits reset to 0")
-
+	// Only allow in dev
 	if api.platform != "dev" {
-		statusCode = http.StatusForbidden
-		w.WriteHeader(statusCode)
-		response = []byte("Reset is only allowed in dev environment.")
-		if _, err := w.Write(response); err != nil {
-			log.Printf("reset: error writing response: %v", err)
-			return
-		}
+		http.Error(w, "Reset is only allowed in dev environment.", http.StatusForbidden)
 		return
 	}
 
 	api.FileserverHits.Store(0)
-	err := api.DB.ResetUsers(r.Context())
-	if err != nil {
-		statusCode = http.StatusInternalServerError
+
+	if err := api.DB.ResetUsers(r.Context()); err != nil {
 		log.Printf("reset: error resetting users: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
 	}
 
-	w.WriteHeader(statusCode)
-	if _, err := w.Write(response); err != nil {
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte("Hits reset to 0")); err != nil {
 		log.Printf("reset: error writing response: %v", err)
-		return
 	}
 }
